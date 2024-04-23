c7a0099f6f540888d378b2546fbd0237e8113a3a
Ikromov2005 <159450064+Ikromov2005@users.noreply.github.com>
2024-04-23 09:26:49 +0500

    Firdt commit

---
+++ app_users/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
+
+from .forms import UserRegistrationForm
+User = get_user_model()
+
+
+def user_registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            if request.POST.get('password1') == request.POST.get('password2'):
+                user = form.save(commit=False)
+                user.set_password(request.POST.get('password1'))
+                user.save()
+                return redirect('login')
+
+    form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'app_users/registration.html', context)
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            user_exists = user.check_password(password)
+        except:
+            user_exists = None
+            user = None
+
+        if user_exists:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'app_users/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
